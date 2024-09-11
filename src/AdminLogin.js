d9d06d39a3da6109a0e30953122b27f740e40cc8
tanisha-7001 <tanisha7001@gmail.com>
2024-09-11 15:34:37 +0530

    6

--- src/AdminLogin.js
+++ src/AdminLogin.js
@@ -1,23 +1,14 @@
-import React from 'react';
-import { Authenticator} from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import AdminDashboard from './AdminDashboard';
 
-const CustomSignIn = (props) => (
-  <Authenticator hideSignUp>
-    {({ signOut }) => (
-      <>
-        <AdminDashboard />
-        <button onClick={signOut}>Sign out</button>
-      </>
-    )}
-  </Authenticator>
-);
-
-function AdminLogin() {
+function AdminLogin({ signOut, user }) {
   return (
-    <CustomSignIn />
+    <>
+      <AdminDashboard/>
+      <button onClick={signOut}>Sign out</button>
+    </>
   );
 }
 
-export default AdminLogin;
+export default withAuthenticator(AdminLogin);
